3d3105ec8b80b226c8767223edcac4ba8ee36f0d
Philipp Spitzley <129886018+defPhisy@users.noreply.github.com>
2024-11-11 17:36:09 +0100

    feat: add 404 error page

--- app.py
+++ app.py
@@ -33,5 +33,10 @@ def delete(post_id):
     return redirect(url_for("index"))
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
